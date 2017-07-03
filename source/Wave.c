c6e342ca0291b0764f073d31853ad86305b97d5c
Chen Xiaofeng <515318881@qq.com>
2017-07-03 11:12:24 +0800

    加上超声驱动

--- source/Wave.c
+++ source/Wave.c
@@ -10,46 +10,38 @@ U32 distance;
 U32 distance_tmp;
 U32 distance_last;
 
-U32 trigtimef;
-U32 trigtime;
-U32 trigtimeus;
-//---- LOCAL ---
-
-// ===== Function Declaration ===== ( Local ) ( No need for users to use)
-
-  // -- Basic Drivers --
-
-  // --  Hardware Interface --
-
-// =======  Function Realization ======
-
-
-void GetDistance()
-{
-  /*  PTC->PSOR |= 1<<16;   //高电位
-    trigtimef=PIT2_VAL();
-    for (int i=0;i<1700;++i)
-    {
-      
-    }
-    trigtime=trigtimef-PIT2_VAL();
-    trigtimeus = trigtime / (g_bus_clock/1000000); //1us
-    PTC->PCOR |= 1<<16;   //低电位*/
+void StartUltrasound(u8 x){
+  if(x)
+    PTB->PSOR |= 1<<23;
+  else
+    PTB->PCOR |= 1<<23;
 }
 
 void Wave_Init()
 {
-  /*  
-  PORTC->PCR[17] |= PORT_PCR_MUX(1) |PORT_PCR_PE_MASK | PORT_PCR_PS_MASK | PORT_PCR_IRQC(11);    // 1 SI     超声波芯片输入  0
-    PORTC->PCR[16] |= PORT_PCR_MUX(1);    // 1 CLK   超声波芯片输出  1
-    PTC->PDDR |= (0x1<<16);
-    NVIC_EnableIRQ(PORTC_IRQn);
-    NVIC_SetPriority(PORTC_IRQn, NVIC_EncodePriority(NVIC_GROUP, 2, 1));
-  */
+  PORTB->PCR[11] |= PORT_PCR_MUX(1) |PORT_PCR_PE_MASK | PORT_PCR_PS_MASK | PORT_PCR_IRQC(11);    // CCD2AO接口用作输入，超声波接收
+  PORTB->PCR[23] |= PORT_PCR_MUX(1);    // CCD1AO接口用作输出，超声波发送
+  PTB->PDDR |= (0x1<<23);
+  NVIC_EnableIRQ(PORTB_IRQn);
+  NVIC_SetPriority(PORTB_IRQn, NVIC_EncodePriority(NVIC_GROUP, 2, 1)); //PORTC中断服务程序在cam.c中
 }
 
-// ======= Basic Drivers ======
-
-
-   
-// ===== Hardware Interface =====
+void PORTB_IRQHandler(){
+  if((PORTB->ISFR)&PORT_ISFR_ISF(1 << 11)){  //超声波接收中断
+    PORTB->ISFR |= PORT_ISFR_ISF(1 << 11);
+    if((PTB->PDIR>>11)&1)  
+    {
+      wavetimef=PIT2_VAL();
+    }
+    else 
+    {
+        wavetime=wavetimef-PIT2_VAL();
+        wavetimeus = wavetime / (g_bus_clock/1000000); //1us
+        distance_tmp=wavetimeus*34/200;    //距离单位毫米
+        if(distance_tmp>(distance_last+100)){}
+        else distance=distance_tmp;
+        
+        distance_last = distance;
+    }
+  }
+}
